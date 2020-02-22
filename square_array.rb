e2d2dd251339fa5ce3cabdc2598148812790ac76
Samantha Barros <samantha.barros95@gmail.com>
2020-02-22 04:08:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(array)
   array = [1,2,3] 
-  arrays = array ** 2
-    array.each do |arrays|
-     # arrays = array ** 2
+    array.each do |arrays**2|
       puts ("#{arrays}")
     end
 end
